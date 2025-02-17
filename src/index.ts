4ede093a372437cd4e9b370eca0f3b6aafa3cd58
ferriyusra <ferisakuragi@gmail.com>
2025-02-17 17:29:13 +0700

    refactor

--- src/index.ts
+++ src/index.ts
@@ -34,29 +34,8 @@ logger.info('Initializing express');
 const app = express();
 
 // Set port
-const port: number = Number(process.env.APP_PORT) || 3000;
+const port: number = Number(process.env.APP_PORT) || 9852;
 app.set('port', port);
-
-function onError(error: NodeJS.ErrnoException): void {
-	if (error.syscall !== 'listen') {
-		throw error;
-	}
-	const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
-
-	// handle specific listen errors with friendly messages
-	switch (error.code) {
-		case 'EACCES':
-			logger.error(bind + ' requires elevated privileges');
-			process.exit(1);
-			break;
-		case 'EADDRINUSE':
-			logger.error(bind + ' is already in use');
-			process.exit(1);
-			break;
-		default:
-			throw error;
-	}
-}
 
 function onListening(server: http.Server): void {
 	const addr = server.address();
